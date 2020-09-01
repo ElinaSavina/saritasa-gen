ac2993c0ad9ab31dbbcb89076058be708691522a
Elina Savina <elina.savina@saritasa.com>
2020-09-01 16:29:31 +0700

    Initialize project structure

---
+++ src/SaritasaGen.FeatureGenerator/DependencyInjection/ServicesAutofacModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace SaritasaGen.FeatureGenerator.DependencyInjection
+{
+    /// <summary>
+    /// Services Autofac module.
+    /// </summary>
+    public class ServicesAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
